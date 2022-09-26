5c7472f8b76409aa86fc14e818acbc34c1026258
Patrick  Wambua <102645955+PatrickNthiwa@users.noreply.github.com>
2022-09-26 09:45:25 +0300

    Add files via upload
    Added the database rental.db

---
+++ rental.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 22, 2022 at 05:40 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tenant`
+--
+
+CREATE TABLE `Tenant` (
+  `fullname` varchar(100) NOT NULL,
+  `PhoneNo` varchar(15) NOT NULL,
+  `email` varchar(109) NOT NULL,
+  `rentalName` varchar(100) NOT NULL,
+  `roomNo` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Tenant`
+--
+ALTER TABLE `Tenant`
+  ADD PRIMARY KEY (`PhoneNo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
